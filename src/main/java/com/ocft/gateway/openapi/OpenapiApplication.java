112779ecc3616f165ccac14fbccec7c39ed5c965
fan <522400740@qq.com>
2025-09-06 13:19:48 +0800

    main init author

--- src/main/java/com/ocft/gateway/openapi/OpenapiApplication.java
+++ src/main/java/com/ocft/gateway/openapi/OpenapiApplication.java
@@ -3,11 +3,15 @@ package com.ocft.gateway.openapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Shaper
+ * 2025-09-06 13:19:24
+ */
 @SpringBootApplication
 public class OpenapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OpenapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OpenapiApplication.class, args);
+    }
 
 }
